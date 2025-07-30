80f64f9fce5952ea47dd6dda9427c7b372bdf5d7
jonathan miquelino <jonathanny94@gmail.com>
2025-07-29 21:03:00 -0300

    primeiro commit

---
+++ ProjetoAulasPoo/Program.cs
@@ -0,0 +1,49 @@
+﻿using ProjetoAulasPOO;
+using System.Globalization;
+
+namespace Application
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo x, y;
+
+            x = new Triangulo();
+            y = new Triangulo();
+
+            Console.WriteLine("Entre com as medidas do triângulo X");
+            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Entre com as medidas do triângulo Y");
+            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            //calculo do p de x
+            double p = (x.A + x.B + x.C) / 2.0;
+            //calculo da area de x
+            double areaX = Math.Sqrt(p * (p - x.A) * (p - x.B) * (p - x.C));
+
+            //calculo do p de y
+            p = (y.A + y.B + y.C) / 2.0;
+            //calculo da area de y
+            double areaY = Math.Sqrt(p * (p - y.A) * (p - y.B) * (p - y.C));
+
+            Console.WriteLine("Área de x = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Área de y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+
+            if (areaX > areaY)
+            {
+                Console.WriteLine("Área de x é maior");
+            }
+            else
+            {
+                Console.WriteLine("Área de y é maior");
+            }
+
+        }
+    }
+}
